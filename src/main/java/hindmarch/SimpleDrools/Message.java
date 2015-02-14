3c0b8dda4c04b39c61d6ecc95b85ce91c09975f3
hindmasj <hindmasj.noreply@fake.address.com>
2015-02-14 22:40:49 +0000

    Begin

---
+++ src/main/java/hindmarch/SimpleDrools/Message.java
@@ -0,0 +1,32 @@
+package hindmarch.SimpleDrools;
+
+public class Message {
+	public static final int UNHANDLED=0;
+	public static final int HANDLED=1;
+	
+	String message;
+	int status;
+	
+	public Message(String message, int status) {
+		super();
+		this.message = message;
+		this.status = status;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ src/main/java/hindmarch/SimpleDrools/Runner.java
@@ -0,0 +1,33 @@
+package hindmarch.SimpleDrools;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.event.rule.DebugAgendaEventListener;
+import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+
+public class Runner {
+	public static final String SESSION="SimpleKS";
+	public static final String BASE="SimpleKB";
+
+	public static void main(String[] args) {
+		KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieSession ksession = kc.newKieSession(SESSION);
+        
+        Runner runner=new Runner();
+		runner.run(ksession);
+	}
+	
+	public void run(KieSession ksession){
+		ksession.addEventListener( new DebugAgendaEventListener() );
+        ksession.addEventListener( new DebugRuleRuntimeEventListener() );
+
+        Message one=new Message("Hello World One",Message.UNHANDLED);
+        
+        ksession.insert(one);
+        ksession.fireAllRules();
+	}
+
+}
